e9c2468a3bafb18d1eacfcf6d1a8d1181bfb69a2
Prince <fakesk009@gmail.com>
2026-09-21 01:21:44 +0000

    Rename core

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from gates.and_gate import run_and_gate
-from gates.or_gate import run_or_gate
+from gate.and_gate import run_and_gate
+from gate.or_gate import run_or_gate
 
 
 def display_results(name, X, y, predictions, model):
